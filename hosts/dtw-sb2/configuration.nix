8b9d79e57619cb74d8d383f224b00732f4c0fb08
David Wood <david@davidtw.co>
2018-05-25 08:46:55 +0100

    Add surface kernel patches, firmware and required configuration.

--- hosts/dtw-sb2/configuration.nix
+++ hosts/dtw-sb2/configuration.nix
@@ -5,6 +5,8 @@
 # for this import to work correctly.
 let
   unstable = import <unstable> {};
+  surface_firmware = import ./firmware-surfacebook2-13in.nix;
+  surface_patches = import ./patches-surface.nix;
 in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -14,6 +16,62 @@ in {
   networking.hostName = "dtw-sb2";
   networking.wireless.enable = true;
 
-  # Patch kernel for Surface Book 2.
+  # Patch the kernel.
   boot.kernelPackages = unstable.linuxPackages_4_16;
+  boot.kernelPatches = [
+    surface_patches.acpica
+    surface_patches.cameras
+    surface_patches.config
+    surface_patches.ipts
+    surface_patches.keyboards_and_covers
+    surface_patches.sdcard_reader
+    surface_patches.surfaceacpi
+    surface_patches.surfacedock
+    surface_patches.wifi
+  ];
+
+  # Enable all the required kernel modules.
+  boot.kernelModules = [
+    "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid"
+    "hid_multitouch" "intel_ipts" "kvm-intel"
+  ];
+  boot.initrd.kernelModules = [ "hid-multitouch" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+
+  # Add extra firmware.
+  hardware.firmware = [ surface_firmware ];
+
+  # Install `libwacom` for the touchscreen.
+  environment.systemPackages = with pkgs; [ libwacom ];
+
+  # Add udev rules for attaching/detaching the keyboard.
+  services.udev.extraRules = "${surface_patches.repo}/root/etc/udev/rules.d/*.rules";
+
+  # Configure hibernation.
+  services.logind.lidSwitch = "hibernate";
+  environment.etc."systemd/sleep.conf".text = ''
+    [Sleep]
+    SuspendState=freeze
+  '';
+
+  # Configure pulseaudio for the speakers.
+  hardware.pulseaudio.daemon.config = {
+    resample-method = "speex-float-3";
+    flat-volumes = "no";
+    default-sample-format = "s24le";
+    default-sample-rate = "44100";
+  };
+
+  # Set up video devices.
+  services.xserver.wacom.enable = true;
+  services.xserver.videoDrivers = [ "intel" "nouveau" ];
+  services.xserver.deviceSection = ''
+    Option "TripleBuffer" "true"
+    Option "TearFree" "true"
+    Option "DRI" "true"
+  '';
+
+  # Set up powersaving wifi.
+  networking.networkmanager.wifi.powersave = true;
+  # }}}
 }
