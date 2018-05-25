9abbf7269f41e0be0b4f55b701b5fc963b249f5d
David Wood <david@davidtw.co>
2018-05-25 08:46:10 +0100

    Enable synaptics and configure trackpad.

--- services/x.nix
+++ services/x.nix
@@ -10,6 +10,21 @@
 
     windowManager.i3.enable = true;
     windowManager.default = "i3";
+
+    synaptics = {
+      enable = true;
+      twoFingerScroll = true;
+      tapButtons = true;
+      accelFactor = "0.001";
+      palmDetect = true;
+      additionalOptions = ''
+        Option "VertScrollDelta" "-180"
+        Option "HorizScrollDelta" "-180"
+        Option "FingerLow" "40"
+        Option "FingerHigh" "70"
+        Option "Resolution" "270"
+      '';
+    };
   };
 
   fonts.fontconfig.dpi = 200;
