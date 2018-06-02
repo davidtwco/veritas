fdfbcef083ccb1904df91169b528e61fa3b64eab
David Wood <david@davidtw.co>
2018-06-02 16:56:37 +0100

    Enabled media keys.

--- services/audio.nix
+++ services/audio.nix
@@ -1,7 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  # Enable audio.
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.support32Bit = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
+
+  # Enable media keys.
+  sound.mediaKeys = {
+    enable = true;
+    volumeStep = "5%";
+  };
 }
