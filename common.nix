0cc351a8babadfa52cdc38372711160babcbc875
David Wood <david@davidtw.co>
2019-05-04 11:05:18 +0100

    Install tree.

--- common.nix
+++ common.nix
@@ -33,7 +33,7 @@
     wget file unzip zip unrar p7zip dmidecode pstree dtrx htop iotop powertop ltrace strace
     linuxPackages.perf pciutils lshw smartmontools usbutils inetutils wireshark
     nix-prefetch-scripts pmutils psmisc which binutils bc exfat dosfstools patchutils moreutils
-    ncdu bmon nix-index exa neofetch mosh pkgconfig direnv cron
+    ncdu bmon nix-index exa neofetch mosh pkgconfig direnv cron tree
 
     # Man pages
     man man-pages posix_man_pages stdman
