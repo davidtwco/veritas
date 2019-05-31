bc6ed19fc84cea09d486be29d75928695cc99b8a
David Wood <david@davidtw.co>
2019-05-31 09:44:49 +0100

    Add inhibit lock for active SSH connections.

--- services/ssh.nix
+++ services/ssh.nix
@@ -14,13 +14,35 @@
     permitRootLogin = "no";
   };
 
-  services.xserver.desktopManager.gnome3.extraGSettingsOverrides =
-    if config.services.xserver.desktopManager.gnome3.enable
-    then ''
-      [org.gnome.settings-daemon.plugins.power]
-      sleep-inactive-ac-type='nothing'
-    ''
-    else null;
+  # Don't sleep or hibernate when there are active SSH connections.
+  environment.etc."ssh/sshrc" = {
+    text = ''
+      #!/usr/bin/env sh
+      export tmp_file="/tmp/systemd-inhibit-suspend.log"
+      if [ ! -z $SSH_TTY ]; then
+        ${pkgs.coreutils}/bin/nohup \
+          ${pkgs.systemd}/bin/systemd-inhibit --what="sleep" --who="sshd" \
+          --why="No sleep due to $SSH_TTY" --mode="delay" \
+          /etc/ssh/systemd_lock.sh \
+          &>> "$tmp_file" &
+      fi
+    '';
+    mode = "744";
+  };
+
+  environment.etc."ssh/systemd_lock.sh" = {
+    text = ''
+      #!/usr/bin/env sh
+      export LANG=C
+      while ${pkgs.coreutils}/bin/who | \
+            ${pkgs.gnugrep}/bin/grep --quiet " $(echo $SSH_TTY | \
+            ${pkgs.coreutils}/bin/cut -d "/" -f 3- ) "; do
+        ${pkgs.coreutils}/bin/sleep 60
+      done
+    '';
+    mode = "744";
+    user = "david";
+  };
 }
 
 # vim:foldmethod=marker:foldlevel=0:ts=2:sts=2:sw=2
