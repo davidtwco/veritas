2f333ed10f40325c7229f97b34fc609f8d7b58d9
David Wood <david@davidtw.co>
2019-06-06 15:25:36 +0100

    Upgrade `intel-ocl` package.

--- packages/intel-ocl.nix
+++ packages/intel-ocl.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "intel-ocl-${version}";
-  version = "18.1.0.013";
+  version = "18.1.0.015";
 
   src = fetchzip {
-    url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13793/l_opencl_p_${version}.tgz";
-    sha256 = "1q2w4ijkf71glyqaf89hvl4z6pfxpsh194mqqd9yx7qcxiwj2bj4";
+    url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/15532/l_opencl_p_${version}.tgz";
+    sha256 = "1ydb3n4600za7hlflbsp5kdip7wgxkhjw5wfvzs8vg01zd3kh4vf";
     stripRoot = false;
   };
 
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
 
   # Extract the RPMs contained within the source archive.
   postUnpack = ''
-    rpmextract ./source/l_opencl_p_${version}/rpm/intel-openclrt-${version}-18.1.0-013.x86_64.rpm
+    rpmextract ./source/l_opencl_p_${version}/rpm/intel-openclrt-${version}-18.1.0-015.x86_64.rpm
   '';
 
   # Remove `libOpenCL.so`, since we use ocl-icd's `libOpenCL.so` instead and this would cause a
