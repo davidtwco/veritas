902c562ce320a7b421e97da9aa076c6a0c1fca1a
David Wood <david@davidtw.co>
2019-06-18 17:44:45 +0100

    Fix mail configuration.

--- services/mail.nix
+++ services/mail.nix
@@ -10,7 +10,7 @@ in {
   networking.defaultMailServer = {
     authUser = "${email}";
     # Set the permissions of this to 600!
-    authPassFile = ../secrets/mail.password;
+    authPassFile = "${../secrets/mail.password}";
     directDelivery = true;
     domain = "${domain}";
     hostName = "smtp.fastmail.com:465";
