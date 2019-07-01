56ce3c3ff20742e241543c51eeb5f0286dfc4242
David Wood <david@davidtw.co>
2019-07-01 15:15:35 +0100

    Upgrade Plex.

--- overlay.nix
+++ overlay.nix
@@ -28,11 +28,11 @@ self: super:
 
   # Install 'Plex Pass' version of Plex.
   plexPassRaw = super.unstable.plexRaw.overrideAttrs (old: rec {
-    version = "1.16.1.1246-1d09ac057";
+    version = "1.16.1.1291-158e5b199";
     name = "${old.pname}-${version}";
     src = super.fetchurl {
       url = "https://downloads.plex.tv/plex-media-server-new/${version}/redhat/plexmediaserver-${version}.x86_64.rpm";
-      sha256 = "1h2g5vm9k6zsp4bdw4gzdkhiq9bh11sma0cifysax9pmxg5p4a0d";
+      sha256 = "0abmky8xvmmwvmpcxx7szdwlq07xb43sbvbv3k72y1yha0nsdqhp";
     };
   });
   plexPass = super.unstable.plex.override { plexRaw = self.plexPassRaw; };
