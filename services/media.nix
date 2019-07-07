c145dde2ef906dc35be71df8ae5d9298d56579b9
David Wood <david@davidtw.co>
2019-07-07 12:47:27 +0100

    Refactor VPN configuration to route multiple users traffic and change VPN servers.

--- services/media.nix
+++ services/media.nix
@@ -6,6 +6,13 @@ let
   vpn = {
     # Path to the certificate for the VPN provider.
     certificate = builtins.fetchurl https://proxy.sh/proxysh.crt;
+    # Names of the chains used in iptables rules.
+    chains = {
+      input = "${group}-vpn-input";
+      output = "${group}-vpn-output";
+      prerouting = "${group}-vpn-prerouting";
+      postrouting = "${group}-vpn-postrouting";
+    };
     # Path to the `auth-user-pass` file for OpenVPN.
     credentials = ../secrets/proxy_sh.credentials;
     # DNS servers to use for VPN traffic.
@@ -29,22 +36,20 @@ let
     # Bit pattern to mark packets with.
     mark = "0x1";
     # Remote servers to use with OpenVPN.
-    remotes = [ "mx.proxy.sh" "nl.proxy.sh" "es.proxy.sh" "ch.proxy.sh" ];
+    remotes = [ "eu.proxy.sh" ];
     # Arbitrary id to identify the routing table for the VPN user, needs to be consistent across
     # a handful of files.
     routeTableId = 42;
     # Names of tunnel interface for OpenVPN.
-    tunnel = "tun-deluge";
-    # User whose traffic should be forced onto the VPN.
-    #
-    # Deluge doesn't let us specify the username, but it is visible from the derivation source that
-    # it is "deluge".
-    user = "deluge";
+    tunnel = "tun-${group}";
+    # Users whose traffic should be forced onto the VPN.
+    users = [ config.services.jackett.user config.services.deluge.user ];
   };
-  # Routing Script {{{
-  # ==============
+  # Name of the group that all services run as.
+  group = "media";
+  # Routing script used by oneshot service and VPN up script.
   routingScript = let
-    name = "${vpn.user}-vpn-routes";
+    name = "${group}-vpn-routes";
     dir = pkgs.writeScriptBin name ''
       #! ${pkgs.runtimeShell} -e
       # Flush routes currently in the table.
@@ -54,7 +59,8 @@ let
       # order (lowest first - see `ip rule list`) and if no routes within match, then the next
       # rule is checked. This rule will be the second rule (the first being local packets) and
       # will apply for the marked packets.
-      HAS_RULE="$(${pkgs.iproute}/bin/ip rule list | ${pkgs.gnugrep}/bin/grep -c ${vpn.mark} || true)"
+      HAS_RULE="$(${pkgs.iproute}/bin/ip rule list | \
+        ${pkgs.gnugrep}/bin/grep -c ${vpn.mark} || true)"
       if [[ $HAS_RULE == "0" ]]; then
         ${pkgs.iproute}/bin/ip rule add from all fwmark ${vpn.mark} \
           lookup ${builtins.toString vpn.routeTableId}
@@ -81,37 +87,29 @@ let
       ${pkgs.iproute}/bin/ip route flush cache
     '';
   in "${dir}/bin/${name}";
-  # }}}
 in
   {
     # Groups {{{
     # ======
-    users.groups.media.members = [
-      "david"
-      config.systemd.services.deluged.serviceConfig."User"
-      config.services.sabnzbd.user
-      config.services.sonarr.user
-      config.services.radarr.user
-      config.services.plex.user
-      config.systemd.services.lidarr.serviceConfig."User"
-    ];
+    users.groups.media.members = [ "david" ] ++ (with config.services; [
+      deluge.user sabnzbd.user sonarr.user radarr.user plex.user lidarr.user
+    ]);
     # }}}
 
     # SABnzbd {{{
     # =======
     services.sabnzbd = {
+      inherit group;
       enable = true;
-      group = "media";
     };
     # }}}
 
     # Deluge {{{
     # =======
     services.deluge = {
+      inherit group;
       enable = true;
-      # Add unrar to the `extraPackages`.
       extraPackages = [ pkgs.unrar ];
-      group = "media";
       web = {
         enable = true;
         openFirewall = true;
@@ -122,8 +120,8 @@ in
     # Jackett {{{
     # =======
     services.jackett = {
+      inherit group;
       enable = true;
-      group = "media";
       openFirewall = true;
       package = pkgs.unstable.jackett;
     };
@@ -132,8 +130,8 @@ in
     # Radarr {{{
     # ======
     services.radarr = {
+      inherit group;
       enable = true;
-      group = "media";
       openFirewall = true;
     };
     # }}}
@@ -141,8 +139,8 @@ in
     # Sonarr {{{
     # ======
     services.sonarr = {
+      inherit group;
       enable = true;
-      group = "media";
       openFirewall = true;
     };
     # }}}
@@ -150,8 +148,8 @@ in
     # Lidarr {{{
     # ======
     services.lidarr = {
+      inherit group;
       enable = true;
-      group = "media";
       openFirewall = true;
       package = pkgs.unstable.lidarr;
     };
@@ -169,7 +167,7 @@ in
     # OpenVPN {{{
     # =======
     services.openvpn.servers = {
-      deluge = {
+      "${group}" = {
         autoStart = true;
         authUserPass = {
           username = lib.removeSuffix "\n" (builtins.readFile ../secrets/proxy_sh.username);
@@ -246,10 +244,10 @@ in
       # chains and rules can apply.
       for table in filter nat mangle; do
         # Delete any existing jumps to the VPN chains.
-        ip46tables -t "$table" -D INPUT -j ${vpn.user}-vpn-input 2> /dev/null || true
-        ip46tables -t "$table" -D OUTPUT -j ${vpn.user}-vpn-output 2> /dev/null || true
+        ip46tables -t "$table" -D INPUT -j ${vpn.chains.input} 2> /dev/null || true
+        ip46tables -t "$table" -D OUTPUT -j ${vpn.chains.output} 2> /dev/null || true
 
-        for chain in ${vpn.user}-vpn-input ${vpn.user}-vpn-output; do
+        for chain in ${vpn.chains.input} ${vpn.chains.output}; do
           # Flush the chain (ignoring errors if it doesn't exist).
           ip46tables -t "$table" -F "$chain" 2> /dev/null || true
           # Delete the chain (ignoring errors if it doesn't exist).
@@ -259,129 +257,142 @@ in
         done
 
         # Add a jump to the VPN chains.
-        ip46tables -t "$table" -A INPUT -j ${vpn.user}-vpn-input
-        ip46tables -t "$table" -A OUTPUT -j ${vpn.user}-vpn-output
+        ip46tables -t "$table" -A INPUT -j ${vpn.chains.input}
+        ip46tables -t "$table" -A OUTPUT -j ${vpn.chains.output}
       done
 
       # Manually handle the prerouting/postrouting chain in the nat table - it doesn't fit the
       # above loop.
-      ip46tables -t nat -D PREROUTING -j ${vpn.user}-vpn-prerouting 2> /dev/null || true
-      ip46tables -t nat -D POSTROUTING -j ${vpn.user}-vpn-postrouting 2> /dev/null || true
-      for chain in ${vpn.user}-vpn-prerouting ${vpn.user}-vpn-postrouting; do
+      ip46tables -t nat -D PREROUTING -j ${vpn.chains.prerouting} 2> /dev/null || true
+      ip46tables -t nat -D POSTROUTING -j ${vpn.chains.postrouting} 2> /dev/null || true
+      for chain in ${vpn.chains.prerouting} ${vpn.chains.postrouting}; do
         ip46tables -t nat -F "$chain" 2> /dev/null || true
         ip46tables -t nat -X "$chain" 2> /dev/null || true
         ip46tables -t nat -N "$chain"
       done
-      ip46tables -t nat -A PREROUTING -j ${vpn.user}-vpn-prerouting
-      ip46tables -t nat -A POSTROUTING -j ${vpn.user}-vpn-postrouting
+      ip46tables -t nat -A PREROUTING -j ${vpn.chains.prerouting}
+      ip46tables -t nat -A POSTROUTING -j ${vpn.chains.postrouting}
 
       # Accept existing connections.
-      ip46tables -t filter -A ${vpn.user}-vpn-input -m conntrack \
-        --ctstate RELATED,ESTABLISHED -j ACCEPT
-      ip46tables -t filter -A ${vpn.user}-vpn-output -m conntrack \
-        --ctstate RELATED,ESTABLISHED -j ACCEPT
+      ip46tables -t filter -A ${vpn.chains.input} -m conntrack --ctstate RELATED,ESTABLISHED \
+        -j ACCEPT
+      ip46tables -t filter -A ${vpn.chains.output} -m conntrack --ctstate RELATED,ESTABLISHED \
+        -j ACCEPT
 
       # Copy connection mark to the packet mark.
-      ip46tables -t mangle -A ${vpn.user}-vpn-output -j CONNMARK --restore-mark
+      ip46tables -t mangle -A ${vpn.chains.output} -j CONNMARK --restore-mark
 
-      # Mark all outgoing packets from the VPN user (these will be unmarked later). An initial mark
-      # followed by an unmark is used because it isn't possible to check for multiple destinations
-      # and negate that check (i.e. `! --dest W.X.Y.Z/F,A.B.C.D/G` is disallowed) and if these
-      # checks were split up then all but one would always be triggered.
-      ip46tables -t mangle -A ${vpn.user}-vpn-output ! -o lo -m owner --uid-owner ${vpn.user} \
-        -j MARK --set-mark ${vpn.mark}
-
-      # Unmark outgoing packets from the VPN user when being sent to a private network. This means
-      # that hosts on the local network will still be able to browse to any web services without
-      # being blocked.
-      iptables -t mangle -A ${vpn.user}-vpn-output --dest "${vpn.ipv4Networks}" \
-        -m owner --uid-owner ${vpn.user} -j MARK --xor-mark ${vpn.mark}
-      ip6tables -t mangle -A ${vpn.user}-vpn-output --dest "${vpn.ipv6Networks}" \
-        -m owner --uid-owner ${vpn.user} -j MARK --xor-mark ${vpn.mark}
-
-      # Re-mark packets on the private network if they are DNS. This forces DNS to go through
-      # the VPN.
       ${
         builtins.concatStringsSep "\n" (
-          builtins.map ({ command, networks }: ''
-            ${command} -t mangle -A ${vpn.user}-vpn-output --dest "${networks}" -p udp \
-              --dport domain -m owner --uid-owner ${vpn.user} -j MARK --set-mark ${vpn.mark}
-            ${command} -t mangle -A ${vpn.user}-vpn-output --dest "${networks}" -p tcp \
-              --dport domain -m owner --uid-owner ${vpn.user} -j MARK --set-mark ${vpn.mark}
-            '') [
-              { command = "iptables"; networks = vpn.ipv4Networks; }
-              { command = "ip6tables"; networks = vpn.ipv6Networks; }
-            ]
+          builtins.map (user: ''
+            # Mark all outgoing packets from the VPN user (these will be unmarked later). An
+            # initial mark followed by an unmark is used because it isn't possible to check for
+            # multiple destinations and negate that check (i.e. `! --dest W.X.Y.Z/F,A.B.C.D/G`
+            # is disallowed) and if these checks were split up then all but one would always be
+            # triggered.
+            ip46tables -t mangle -A ${vpn.chains.output} ! -o lo -m owner --uid-owner ${user} \
+              -j MARK --set-mark ${vpn.mark}
+
+            # Unmark outgoing packets from the VPN user when being sent to a private network.
+            # This means that hosts on the local network will still be able to browse to any web
+            # services without being blocked.
+            iptables -t mangle -A ${vpn.chains.output} --dest "${vpn.ipv4Networks}" \
+              -m owner --uid-owner ${user} -j MARK --xor-mark ${vpn.mark}
+            ip6tables -t mangle -A ${vpn.chains.output} --dest "${vpn.ipv6Networks}" \
+              -m owner --uid-owner ${user} -j MARK --xor-mark ${vpn.mark}
+
+            # Re-mark packets on the private network if they are DNS. This forces DNS to go through
+            # the VPN.
+            ${
+              builtins.concatStringsSep "\n" (
+                builtins.map ({ command, networks }: ''
+                  ${command} -t mangle -A ${vpn.chains.output} --dest "${networks}" -p udp \
+                    --dport domain -m owner --uid-owner ${user} -j MARK --set-mark ${vpn.mark}
+                  ${command} -t mangle -A ${vpn.chains.output} --dest "${networks}" -p tcp \
+                    --dport domain -m owner --uid-owner ${user} -j MARK --set-mark ${vpn.mark}
+                  '') [
+                    { command = "iptables"; networks = vpn.ipv4Networks; }
+                    { command = "ip6tables"; networks = vpn.ipv6Networks; }
+                  ]
+              )
+            }
+          '') vpn.users
         )
       }
 
       # Copy packet mark to the connection mark.
-      ip46tables -t mangle -A ${vpn.user}-vpn-output -j CONNMARK --save-mark
+      ip46tables -t mangle -A ${vpn.chains.output} -j CONNMARK --save-mark
 
       # Accept everything incoming on the VPN interface.
-      ip46tables -t filter -A ${vpn.user}-vpn-input -i ${vpn.tunnel} -j ACCEPT
+      ip46tables -t filter -A ${vpn.chains.input} -i ${vpn.tunnel} -j ACCEPT
 
       # Make any DNS from the VPN user go to the VPN provider's DNS.
       ${
         builtins.concatStringsSep "\n" (
-          builtins.map ({ command, networks, dns, protocol }: ''
-            ${command} -t nat -A ${vpn.user}-vpn-output --dest "${networks}" \
-              -p ${protocol} --dport domain -m owner --uid-owner ${vpn.user} -j DNAT \
-              --to-destination ${dns.primary}
-            ${command} -t nat -A ${vpn.user}-vpn-output --dest "${networks}" \
-              -p ${protocol} --dport domain -m owner --uid-owner ${vpn.user} -j DNAT \
-              --to-destination ${dns.secondary}
-          '') [
-            {
-              command = "iptables";
-              networks = vpn.ipv4Networks;
-              dns = vpn.dns.ipv4;
-              protocol = "tcp";
+          builtins.map (user: ''
+            ${
+              builtins.concatStringsSep "\n" (
+                builtins.map ({ command, networks, dns, protocol }: ''
+                  ${command} -t nat -A ${vpn.chains.output} --dest "${networks}" \
+                    -p ${protocol} --dport domain -m owner --uid-owner ${user} -j DNAT \
+                    --to-destination ${dns.primary}
+                  ${command} -t nat -A ${vpn.chains.output} --dest "${networks}" \
+                    -p ${protocol} --dport domain -m owner --uid-owner ${user} -j DNAT \
+                    --to-destination ${dns.secondary}
+                '') [
+                  {
+                    command = "iptables";
+                    networks = vpn.ipv4Networks;
+                    dns = vpn.dns.ipv4;
+                    protocol = "tcp";
+                  }
+                  {
+                    command = "iptables";
+                    networks = vpn.ipv4Networks;
+                    dns = vpn.dns.ipv4;
+                    protocol = "udp";
+                  }
+                  {
+                    command = "ip6tables";
+                    networks = vpn.ipv6Networks;
+                    dns = vpn.dns.ipv6;
+                    protocol = "tcp";
+                  }
+                  {
+                    command = "ip6tables";
+                    networks = vpn.ipv6Networks;
+                    dns = vpn.dns.ipv6;
+                    protocol = "udp";
+                  }
+                ]
+              )
             }
-            {
-              command = "iptables";
-              networks = vpn.ipv4Networks;
-              dns = vpn.dns.ipv4;
-              protocol = "udp";
-            }
-            {
-              command = "ip6tables";
-              networks = vpn.ipv6Networks;
-              dns = vpn.dns.ipv6;
-              protocol = "tcp";
-            }
-            {
-              command = "ip6tables";
-              networks = vpn.ipv6Networks;
-              dns = vpn.dns.ipv6;
-              protocol = "udp";
-            }
-          ]
+
+            # Accept all outgoing packets on `lo` and the VPN interface from the VPN user.
+            ip46tables -t filter -A ${vpn.chains.output} -o lo -m owner --uid-owner ${user} \
+              -j ACCEPT
+            ip46tables -t filter -A ${vpn.chains.output} -o ${vpn.tunnel} -m owner \
+              --uid-owner ${user} -j ACCEPT
+          '') vpn.users
         )
       }
 
-      # Accept all outgoing packets on `lo` and the VPN interface from the VPN user.
-      ip46tables -t filter -A ${vpn.user}-vpn-output -o lo -m owner --uid-owner ${vpn.user} \
-        -j ACCEPT
-      ip46tables -t filter -A ${vpn.user}-vpn-output -o ${vpn.tunnel} -m owner \
-        --uid-owner ${vpn.user} -j ACCEPT
-
       # Masquerade all packets on the VPN interface.
-      ip46tables -t nat -A ${vpn.user}-vpn-postrouting -o ${vpn.tunnel} -j MASQUERADE
+      ip46tables -t nat -A ${vpn.chains.postrouting} -o ${vpn.tunnel} -j MASQUERADE
     '';
 
     networking.iproute2 = {
       enable = true;
       rttablesExtraConfig = ''
-        ${builtins.toString vpn.routeTableId} ${vpn.user}
+        ${builtins.toString vpn.routeTableId} ${group}
       '';
     };
 
     # Create a service that runs on system start, so that the rule to `/dev/null` all marked
     # packets always applies. The same script will be invoked when OpenVPN starts to add a new
     # route that will go through the VPN.
-    systemd.services."${vpn.user}-vpn-routes" = {
-      description = "Routing for ${vpn.user} VPN";
+    systemd.services."${group}-vpn-routes" = {
+      description = "Routing for ${group} VPN";
       wantedBy = [ "sysinit.target" ];
       wants = [ "network-pre.target" ];
       before = [ "network-pre.target" ];
