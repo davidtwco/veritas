1c94916ffbfbf44f7196e18037cf115dd59ed725
David Wood <david@davidtw.co>
2019-08-18 23:08:01 +0100

    Add .Xresources configuration.

--- users/david/home.nix
+++ users/david/home.nix
@@ -380,6 +380,32 @@ in {
   # ===
   xdg.enable = true;
   # }}}
+
+  # Xresources {{{
+  # ==========
+  xresources.properties = {
+    # Hybrid colour scheme.
+    "*background" = "#1C1C1C";
+    "*foreground" = "#C5C8C6";
+    "*cursorColor" = "#C5C8C6";
+    "*color0" = "#282A2E";
+    "*color1" = "#A54242";
+    "*color2" = "#8C9440";
+    "*color3" = "#DE935F";
+    "*color4" = "#5F819D";
+    "*color5" = "#85678F";
+    "*color6" = "#5E8D87";
+    "*color7" = "#707880";
+    "*color8" = "#373B41";
+    "*color9" = "#CC6666";
+    "*color10" = "#B5BD68";
+    "*color11" = "#F0C674";
+    "*color12" = "#81A2BE";
+    "*color13" = "#B294BB";
+    "*color14" = "#8ABEB7";
+    "*color15" = "#C5C8C6";
+  };
+  # }}}
 }
 
 # vim:foldmethod=marker:foldlevel=0:ts=2:sts=2:sw=2:nowrap
