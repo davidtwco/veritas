bd8b3605c75340e164838b1047fef4d68c368b9b
David Wood <david@davidtw.co>
2019-08-19 11:12:21 +0100

    Port previous gpg configuration.

--- users/david/home.nix
+++ users/david/home.nix
@@ -227,83 +227,111 @@ in {
 
   # GnuPG {{{
   # =====
-  programs.gpg = {
-    enable = false;
-    settings = {
-      # Set default key.
-      "default-key" = "9F53F154";
-      # Disable inclusion of the version string in ASCII armored output.
-      "no-emit-version" = true;
-      # Use armoured output.
-      "armor" = true;
-      # Disable comment string in clear text signatures and ASCII armored messages.
-      "no-comments" = true;
-      # Display long key IDs.
-      "keyid-format" = "0xlong";
-      # List all keys (or the specified ones) along with their fingerprints.
-      "with-fingerprint" = true;
-      # Display the calculated validity of user IDs during key listings.
-      "list-options" = "show-uid-validity";
-      "verify-options" = "show-uid-validity";
-      # Try to use the GnuPG-Agent. With this option, GnuPG first tries to connect to the agent
-      # before it asks for a passphrase.
-      "use-agent" = true;
-      # Use unicode.
-      "charset" = "utf-8";
-      # Cross-certify subkeys are present and valid.
-      "require-cross-certification" = true;
-      # Disable caching of passphrase for symmetrical operations.
-      "no-symkey-cache" = true;
-      # Disable putting recipient key IDs into messages.
-      "throw-keyids" = true;
-      # This is the server that --recv-keys, --send-keys, and --search-keys will communicate with
-      # to receive keys from, send keys to, and search for keys on.
-      "keyserver" = "hkps://hkps.pool.sks-keyservers.net";
-      # When using --refresh-keys, if the key in question has a preferred keyserver URL, then
-      # disable use of that preferred keyserver to refresh the key from.
-      "keyserver-options" = "no-honor-keyserver-url";
-      # When searching for a key with --search-keys, include keys that are marked on the keyserver
-      # as revoked.
-      # "keyserver-options" = "include-revoked";
-      # List of personal digest preferences. When multiple digests are supported by all recipients,
-      # choose the strongest one.
-      "personal-cipher-preferences" = "AES256 AES192 AES";
-      # List of personal digest preferences. When multiple ciphers are supported by all recipients,
-      # choose the strongest one.
-      "personal-digest-preferences" = "SHA512 SHA384 SHA256";
-      # Use ZLIB, BZIP2, ZIP, or no compression.
-      "personal-compress-preferences" = "ZLIB BZIP2 ZIP Uncompressed";
-      # Message digest algorithm used when signing a key.
-      "cert-digest-algo" = "SHA512";
-      # SHA512 as digest for symmetric operations.
-      "s2k-digest-algo" = "SHA512";
-      # AES256 as cipher for symmetric operations.
-      "s2k-cipher-algo" = "AES256";
-      # This preference list is used for new keys and becomes the default for "setpref" in the edit
-      # menu.
-      "default-preference-list" =
-        "SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
-    };
-  };
+  # Don't use `programs.gpg` or `services.gpg-agent` until we can find a way to make them work
+  # with agent forwarding.
+  home.file.".gnupg/gpg.conf".text = ''
+    default-key 9F53F154
 
-  services.gpg-agent = {
-    defaultCacheTtl = 600;
-    defaultCacheTtlSsh = config.services.gpg-agent.defaultCacheTtl;
-    enable = false;
-    enableExtraSocket = true;
-    enableScDaemon = true;
-    enableSshSupport = true;
-    extraConfig = ''
-      # Enable logging to a socket for debugging:
-      #   `watchgnupg --time-only --force /home/david/.gnupg/S.log`
-      # verbose
-      # debug-level = guru;
-      # log-file = socket:////home/david/.gnupg/S.log
-    '';
-    grabKeyboardAndMouse = true;
-    maxCacheTtl = 7200;
-    maxCacheTtlSsh = config.services.gpg-agent.maxCacheTtl;
-  };
+    # Disable inclusion of the version string in ASCII armored output.
+    no-emit-version
+
+    # Use armoured output.
+    armor
+
+    # Disable comment string in clear text signatures and ASCII armored messages.
+    no-comments
+
+    # Display long key IDs.
+    keyid-format 0xlong
+
+    # List all keys (or the specified ones) along with their fingerprints.
+    with-fingerprint
+
+    # Display the calculated validity of user IDs during key listings.
+    list-options show-uid-validity
+    verify-options show-uid-validity
+
+    # Try to use the GnuPG-Agent. With this option, GnuPG first tries to connect to
+    # the agent before it asks for a passphrase.
+    use-agent
+
+    # Use unicode.
+    charset utf-8
+
+    # Cross-certify subkeys are present and valid.
+    require-cross-certification
+
+    # Disable caching of passphrase for symmetrical operations.
+    no-symkey-cache
+
+    # Disable putting recipient key IDs into messages.
+    throw-keyids
+
+    # This is the server that --recv-keys, --send-keys, and --search-keys will
+    # communicate with to receive keys from, send keys to, and search for keys on.
+    keyserver hkps://hkps.pool.sks-keyservers.net
+
+    # When using --refresh-keys, if the key in question has a preferred keyserver
+    # URL, then disable use of that preferred keyserver to refresh the key from.
+    keyserver-options no-honor-keyserver-url
+
+    # When searching for a key with --search-keys, include keys that are marked on
+    # the keyserver as revoked.
+    keyserver-options include-revoked
+
+    # List of personal digest preferences. When multiple digests are supported by
+    # all recipients, choose the strongest one.
+    personal-cipher-preferences AES256 AES192 AES
+
+    # List of personal digest preferences. When multiple ciphers are supported by
+    # all recipients, choose the strongest one.
+    personal-digest-preferences SHA512 SHA384 SHA256
+
+    # Use ZLIB, BZIP2, ZIP, or no compression.
+    personal-compress-preferences ZLIB BZIP2 ZIP Uncompressed
+
+    # Message digest algorithm used when signing a key.
+    cert-digest-algo SHA512
+
+    # SHA512 as digest for symmetric operations.
+    s2k-digest-algo SHA512
+
+    # AES256 as cipher for symmetric operations.
+    s2k-cipher-algo AES256
+
+    # This preference list is used for new keys and becomes the default for
+    # "setpref" in the edit menu.
+    default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed
+  '';
+
+  home.file.".gnupg/gpg-agent.conf".text = let
+    pinentry = "${pkgs.pinentry}/bin/" + (if headless then "pinentry-tty" else "pinentry-gnome3");
+  in ''
+    # Wait an hour before prompting again, always
+    # prompt if it has been 2 hours, regardless most
+    # recent use.
+    default-cache-ttl 600
+    max-cache-ttl 7200
+
+    # Don't prompt for ssh. This is primarily so that
+    # async repository checks by prompts don't trigger
+    # random pinentry prompts.
+    default-cache-ttl-ssh 600
+    max-cache-ttl-ssh 7200
+
+    # Act as an SSH agent.
+    enable-ssh-support
+
+    # Use different pinentry script depending on what is available.
+    # Redirect through a script so this works on all distros.
+    pinentry-program ${pinentry}
+
+    # Enable logging to a socket for debugging.
+    # `watchgnupg --time-only --force ${config.home.homeDirectory}/.gnupg/S.log`
+    # verbose
+    # debug-level guru
+    # log-file socket:///${config.home.homeDirectory}/.gnupg/S.log
+  '';
   # }}}
 
   # inputrc {{{
