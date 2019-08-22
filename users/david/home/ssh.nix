6b67b576a2f5d0c66b932ba03f3548254ba69d1f
David Wood <david@davidtw.co>
2019-08-22 19:24:48 +0100

    Allow ControlMaster for github.com in SSH config

--- users/david/home/ssh.nix
+++ users/david/home/ssh.nix
@@ -43,7 +43,6 @@
     in {
       "github.com" = {
         extraOptions = {
-          "ControlMaster" = "no";
           "MACs" = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com";
         };
         identitiesOnly = true;
