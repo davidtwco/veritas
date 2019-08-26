d4b57a66280533250bc72ca462683eda1ee94a91
David Wood <david@davidtw.co>
2019-08-26 18:40:43 +0000

    Add new Ubuntu dotfiles-only host.

---
+++ hosts/wallach.dtw.io.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+
+# This host is an Ubuntu host and only uses Nix for dotfiles, symlink this to `home.nix`.
+
+{
+  # Import the main home-manager configuration.
+  imports = [ ../users/david/home ];
+
+  # Add the `veritas.david` configuration options. These are also used from NixOS.
+  options.veritas.david = import ../users/david/options.nix { inherit config; inherit lib; };
+}
+
+# vim:foldmethod=marker:foldlevel=0:ts=2:sts=2:sw=2:nowrap
