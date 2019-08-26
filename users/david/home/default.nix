5d13a534fbe09e94c2f8763bd24adc00d473a6e0
David Wood <david@davidtw.co>
2019-08-26 18:13:59 +0100

    Re-organize home-manager packages.
    Fixes #11.

--- users/david/home/default.nix
+++ users/david/home/default.nix
@@ -438,42 +438,122 @@ in {
   # Packages {{{
   # ========
   home.packages = with pkgs; [
-    # General utilities
-    file which hyperfine tokei cron direnv moreutils wget bc pkgconfig idutils keepassxc tree
-
-    # NixOS utilities
-    nix-prefetch-scripts nix-index lorri nixfmt nix-review
-
-    # Archiving
-    unzip zip unrar p7zip dtrx
-
-    # Processes/debugging/monitoring
-    htop iotop powertop ltrace strace binutils lshw linuxPackages.perf pciutils psmisc
-    pmutils dmidecode usbutils
-
-    # Networking
-    inetutils mosh bmon bind conntrack-tools tcpdump ethtool linuxPackages.bpftrace
-
-    # Disks
-    parted exfat dosfstools ncdu smartmontools
-
+    # Determine file type.
+    file
+    # Show full path of shell commands.
+    which
+    # Daemon to execute scheduled commands.
+    cron
+    # Collection of useful tools that aren't coreutils.
+    moreutils
+    # Non-interactive network downloader.
+    wget
+    # Return metainformation about installed libraries.
+    pkgconfig
+    # Tools for querying id database.
+    idutils
+    # List directory contents in tree-like format.
+    tree
+    # Interactive process viewer.
+    htop
+    # Top-like I/O monitor.
+    iotop
+    # Power consumption and management diagnosis tool.
+    powertop
+    # Library call tracer.
+    ltrace
+    # System call tracer.
+    strace
+    # Tools for manipulating binaries.
+    binutils
+    # List hardware.
+    lshw
+    # Performance analysis tools.
+    linuxPackages.perf
+    # Collection of programs for inspecting/manipulating configuration of PCI devices.
+    pciutils
+    # Collection of utilities using proc filesystem (`pstree`, `killall`, etc.)
+    psmisc
+    # DMI table decoder.
+    dmidecode
+    # Tools for working with usb devices (`lsusb`, etc.)
+    usbutils
+    # Collection of common network programs.
+    inetutils
+    # Mobile shell with roaming and intelligent local echo.
+    mosh
+    # Bandwidth monitor and rate estimator.
+    bmon
+    # DNS server (provides `dig`)
+    bind
+    # Connection tracking userspace tools.
+    conntrack-tools
+    # Dump traffic on a network.
+    tcpdump
+    # Query/control network driver and hardware settings.
+    ethtool
+    # eBPF tracing language and frontend.
+    linuxPackages.bpftrace
+    # Parititon manipulation program.
+    parted
+    # exFAT filesystem implementation.
+    exfat
+    # Utilities for creating/checking FAT/VFAT filesystems.
+    dosfstools
+    # ncurses disk usage.
+    ncdu
+    # Hard-drive health monitoring.
+    smartmontools
+    # Compress/uncompress `.zip` files.
+    unzip zip
+    # Uncompress `.rar` files.
+    unrar
+    # Compress/uncompress `.7z` files.
+    p7zip
     # Man pages
     man man-pages posix_man_pages stdman
-
-    # Dotfiles
-    fasd universal-ctags ripgrep exa neofetch
-
-    # Version Control
-    gitAndTools.hub gitAndTools.git-imerge patchutils mercurial bazaar subversion
-
+    # Benchmarking.
+    hyperfine
+    # Codebase statistics.
+    tokei
+    # Source `.envrc` when entering a directory.
+    direnv
+    # Improved nix-shell.
+    lorri
+    # Arbitrary-precision calculator.
+    bc
+    # Password manager.
+    keepassxc
+    # Copy files/archives/repositories into the nix store.
+    nix-prefetch-scripts
+    # Index the nix store (provides `nix-locate`).
+    nix-index
+    # Eases nixpkgs review workflow.
+    nix-review
+    # grep alternative.
+    ripgrep
+    # ls alternative.
+    exa
+    # cat alternative.
+    bat
+    # Git wrapper that provides GitHub specific commands.
+    gitAndTools.hub
+    # Quicker access to files and directories.
+    fasd
+    # Incremental git merging/rebasing.
+    gitAndTools.git-imerge
+    # Tools for manipulating patch files.
+    patchutils
+    # Alternative version control systems.
+    mercurial bazaar subversion
     # GnuPG
-    haskellPackages.hopenpgp-tools gnupg
-
+    gnupg
     # Keybase
     keybase
-
-    # Development environment
-    tmate silver-searcher tmate gist python37Packages.pip python3Full workman
+    # Utility for creating gists from stdout.
+    gist
+    # Personal project for managing working directories.
+    workman
   ];
   # }}}
 
