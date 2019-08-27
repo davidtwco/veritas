85052f146d2b2493e1ef424051e8d013d05c456f
David Wood <david@davidtw.co>
2019-08-27 19:39:07 +0100

    Use programs.gpg again. Fixes #4.

--- users/david/home/default.nix
+++ users/david/home/default.nix
@@ -228,82 +228,65 @@ in {
 
   # GnuPG {{{
   # =====
-  # Don't use `programs.gpg` or `services.gpg-agent` until we can find a way to make them work
-  # with agent forwarding.
-  home.file.".gnupg/gpg.conf".text = ''
-    default-key ${config.programs.git.signing.key}
-
-    # Disable inclusion of the version string in ASCII armored output.
-    no-emit-version
-
-    # Use armoured output.
-    armor
-
-    # Disable comment string in clear text signatures and ASCII armored messages.
-    no-comments
-
-    # Display long key IDs.
-    keyid-format 0xlong
-
-    # List all keys (or the specified ones) along with their fingerprints.
-    with-fingerprint
-
-    # Display the calculated validity of user IDs during key listings.
-    list-options show-uid-validity
-    verify-options show-uid-validity
-
-    # Try to use the GnuPG-Agent. With this option, GnuPG first tries to connect to
-    # the agent before it asks for a passphrase.
-    use-agent
-
-    # Use unicode.
-    charset utf-8
-
-    # Cross-certify subkeys are present and valid.
-    require-cross-certification
-
-    # Disable caching of passphrase for symmetrical operations.
-    no-symkey-cache
-
-    # Disable putting recipient key IDs into messages.
-    throw-keyids
-
-    # This is the server that --recv-keys, --send-keys, and --search-keys will
-    # communicate with to receive keys from, send keys to, and search for keys on.
-    keyserver hkps://hkps.pool.sks-keyservers.net
-
-    # When using --refresh-keys, if the key in question has a preferred keyserver
-    # URL, then disable use of that preferred keyserver to refresh the key from.
-    keyserver-options no-honor-keyserver-url
-
-    # When searching for a key with --search-keys, include keys that are marked on
-    # the keyserver as revoked.
-    keyserver-options include-revoked
-
-    # List of personal digest preferences. When multiple digests are supported by
-    # all recipients, choose the strongest one.
-    personal-cipher-preferences AES256 AES192 AES
-
-    # List of personal digest preferences. When multiple ciphers are supported by
-    # all recipients, choose the strongest one.
-    personal-digest-preferences SHA512 SHA384 SHA256
-
-    # Use ZLIB, BZIP2, ZIP, or no compression.
-    personal-compress-preferences ZLIB BZIP2 ZIP Uncompressed
-
-    # Message digest algorithm used when signing a key.
-    cert-digest-algo SHA512
-
-    # SHA512 as digest for symmetric operations.
-    s2k-digest-algo SHA512
-
-    # AES256 as cipher for symmetric operations.
-    s2k-cipher-algo AES256
-
-    # This preference list is used for new keys and becomes the default for
-    # "setpref" in the edit menu.
-    default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed
-  '';
+  # Don't use `services.gpg-agent` until we can find a way to make it work with agent forwarding.
+  programs.gpg = {
+    enable = true;
+    settings = {
+      # Set default key.
+      "default-key" = config.programs.git.signing.key;
+      # Disable inclusion of the version string in ASCII armored output.
+      "no-emit-version" = true;
+      # Use armoured output.
+      "armor" = true;
+      # Disable comment string in clear text signatures and ASCII armored messages.
+      "no-comments" = true;
+      # Display long key IDs.
+      "keyid-format" = "0xlong";
+      # List all keys (or the specified ones) along with their fingerprints.
+      "with-fingerprint" = true;
+      # Display the calculated validity of user IDs during key listings.
+      "list-options" = "show-uid-validity";
+      "verify-options" = "show-uid-validity";
+      # Try to use the GnuPG-Agent. With this option, GnuPG first tries to connect to the agent
+      # before it asks for a passphrase.
+      "use-agent" = true;
+      # Use unicode.
+      "charset" = "utf-8";
+      # Cross-certify subkeys are present and valid.
+      "require-cross-certification" = true;
+      # Disable caching of passphrase for symmetrical operations.
+      "no-symkey-cache" = true;
+      # Disable putting recipient key IDs into messages.
+      "throw-keyids" = true;
+      # This is the server that --recv-keys, --send-keys, and --search-keys will communicate with
+      # to receive keys from, send keys to, and search for keys on.
+      "keyserver" = "hkps://hkps.pool.sks-keyservers.net";
+      # no-honor-keyserver-url: When using --refresh-keys, if the key in question has a preferred
+      #                         keyserver URL, then disable use of that preferred keyserver to
+      #                         refresh the key from.
+      # include-revoked: When searching for a key with --search-keys, include keys that are marked
+      #                  on the keyserver as revoked.
+      "keyserver-options" = "no-honor-keyserver-url include-revoked";
+      # List of personal digest preferences. When multiple digests are supported by all recipients,
+      # choose the strongest one.
+      "personal-cipher-preferences" = "AES256 AES192 AES";
+      # List of personal digest preferences. When multiple ciphers are supported by all recipients,
+      # choose the strongest one.
+      "personal-digest-preferences" = "SHA512 SHA384 SHA256";
+      # Use ZLIB, BZIP2, ZIP, or no compression.
+      "personal-compress-preferences" = "ZLIB BZIP2 ZIP Uncompressed";
+      # Message digest algorithm used when signing a key.
+      "cert-digest-algo" = "SHA512";
+      # SHA512 as digest for symmetric operations.
+      "s2k-digest-algo" = "SHA512";
+      # AES256 as cipher for symmetric operations.
+      "s2k-cipher-algo" = "AES256";
+      # This preference list is used for new keys and becomes the default for "setpref" in the edit
+      # menu.
+      "default-preference-list" =
+        "SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+    };
+  };
 
   home.file.".gnupg/gpg-agent.conf".text = let
     pinentry = "${pkgs.pinentry}/bin/" +
