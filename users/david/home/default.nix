396dab7d20fd8e8a0da69b26e68465da77087b9f
David Wood <david@davidtw.co>
2019-08-27 18:35:28 +0100

    Make .hushlogin on NixOS too.

--- users/david/home/default.nix
+++ users/david/home/default.nix
@@ -113,7 +113,7 @@ in {
 
   # hushlogin {{{
   # =========
-  home.file.".hushlogin".text = lib.mkIf cfg.dotfiles.isNonNixOS ''
+  home.file.".hushlogin".text = ''
     # The mere presence of this file in the home directory disables the system
     # copyright notice, the date and time of the last login, the message of the
     # day as well as other information that may otherwise appear on login.
