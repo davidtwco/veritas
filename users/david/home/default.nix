ae9e37a2531b9e0a751b940b9754b5f6a718ec48
David Wood <david@davidtw.co>
2019-08-27 09:17:04 +0000

    Add .hushlogin on non-NixOS hosts.

--- users/david/home/default.nix
+++ users/david/home/default.nix
@@ -107,6 +107,16 @@ in {
   };
   # }}}
 
+  # hushlogin {{{
+  # =========
+  home.file.".hushlogin".text = lib.mkIf cfg.dotfiles.isNonNixOS ''
+    # The mere presence of this file in the home directory disables the system
+    # copyright notice, the date and time of the last login, the message of the
+    # day as well as other information that may otherwise appear on login.
+    # See `man login`.
+  '';
+  # }}}
+
   # Git {{{
   # ===
   xdg.dataFile."git/template" = {
