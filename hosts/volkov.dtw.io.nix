b755a3c5dd618bbaa5f2a0320122e09ad6cc90f3
David Wood <david@davidtw.co>
2019-09-07 13:06:11 +0100

    Upgrade franz with override.

--- hosts/volkov.dtw.io.nix
+++ hosts/volkov.dtw.io.nix
@@ -83,7 +83,7 @@
   environment.systemPackages = [
     (pkgs.makeAutostartItem { name = "alacritty"; package = pkgs.unstable.alacritty; })
     (pkgs.makeAutostartItem { name = "firefox"; package = pkgs.firefox; })
-    (pkgs.makeAutostartItem { name = "franz"; package = pkgs.unstable.franz; })
+    (pkgs.makeAutostartItem { name = "franz"; package = pkgs.franz; })
   ];
   # }}}
 
