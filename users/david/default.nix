def60d6129be8f70ccef324f4ddbe4ee69ee8fd1
David Wood <david@davidtw.co>
2019-12-16 18:35:37 +0000

    Switch from keys to keyFiles. Fixes #27.

--- users/david/default.nix
+++ users/david/default.nix
@@ -41,9 +41,9 @@ in
       ];
       hashedPassword = "$6$kvMx6lEzQPhkSj8E$KfP/qM2cMz5VqNszLjeOBGnny3PdIyy0vnHzIgP.gb1XqTI/qq3nbt0Qg871pkmwJwIu3ZGt57yShMjFFMR3x1";
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        (builtins.readFile ./public_keys/id_ecdsa_legacy.pub)
-        (builtins.readFile ./public_keys/id_rsa_yubikey.pub)
+      openssh.authorizedKeys.keyFiles = [
+        ./public_keys/id_ecdsa_legacy.pub
+        ./public_keys/id_rsa_yubikey.pub
       ];
       # `shell` attribute cannot be removed! If no value is present then there will be no shell
       # configured for the user and SSH will not allow logins!
