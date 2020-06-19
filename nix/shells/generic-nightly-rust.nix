1ffe95cf2f56b27d9ece129d7d1a1156f9011487
David Wood <david@davidtw.co>
2020-06-19 20:14:56 +0100

    Add bindgen dependencies to generic nightly Rust shell.

--- nix/shells/generic-nightly-rust.nix
+++ nix/shells/generic-nightly-rust.nix
@@ -14,10 +14,19 @@ pkgs.mkShell {
     pkg-config
     libxml2
     openssl
+
+    # Required by bindgen
+    clang
+    clang-tools
+    llvmPackages.llvm
+    llvmPackages.libclang.lib
   ];
 
   # Always show backtraces.
   RUST_BACKTRACE = 1;
+
+  # Set `LIBCLANG_PATH` for bindgen.
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 }
 
 # vim:foldmethod=marker:foldlevel=0:ts=2:sts=2:sw=2:nowrap
