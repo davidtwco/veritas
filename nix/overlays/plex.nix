7eae0804c9106b1b01e307bae4005b0e61563e36
David Wood <david@davidtw.co>
2020-06-24 16:20:39 +0100

    Upgrade Plex to 1.19.4.2935.

--- nix/overlays/plex.nix
+++ nix/overlays/plex.nix
@@ -3,11 +3,11 @@ self: super:
 {
   plexPassRaw = super.plexRaw.overrideAttrs (
     old: rec {
-      version = "1.19.4.2902-69560ce1e";
+      version = "1.19.4.2935-79e214ead";
       name = "${old.pname}-${version}";
       src = super.fetchurl {
         url = "https://downloads.plex.tv/plex-media-server-new/${version}/redhat/plexmediaserver-${version}.x86_64.rpm";
-        hash = "sha256-kH+GAXyYHxyp1OBvBw4N1jNis0SQLLkqvGNwXQyXHas=";
+        hash = "sha256-BGxNGAZL+Uyk7dKzrFuRgQng4XY9903jyt4qmVBu63M=";
       };
     }
   );
