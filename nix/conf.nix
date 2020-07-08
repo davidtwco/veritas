3c6ee2a32f008d9d8c68f89fe51aa4e67c2af9b5
David Wood <david@davidtw.co>
2020-07-08 16:17:41 +0100

    Add pre-commit-hooks cachix build cache.

--- nix/conf.nix
+++ nix/conf.nix
@@ -4,11 +4,13 @@
   binaryCaches = [
     "https://cachix.cachix.org"
     "https://srid.cachix.org"
+    "https://pre-commit-hooks.cachix.org"
   ];
 
   binaryCachePublicKeys = [
     "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
     "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
+    "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
   ];
 }
 
