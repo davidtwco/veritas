3de1e6d0187d77c6ed3a76b2c609cda0f334fd75
David Wood <david@davidtw.co>
2020-07-14 11:39:35 +0100

    Add veritas binary cache.

--- nix/conf.nix
+++ nix/conf.nix
@@ -3,12 +3,14 @@
 {
   binaryCaches = [
     "https://cachix.cachix.org"
+    "https://veritas.cachix.org"
     "https://srid.cachix.org"
     "https://pre-commit-hooks.cachix.org"
   ];
 
   binaryCachePublicKeys = [
     "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+    "veritas.cachix.org-1:jEZ72TVmLDF3j3L8vG7llByY/cSErbL87sVGuHra9xI="
     "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
     "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
   ];
