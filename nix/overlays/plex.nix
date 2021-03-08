6f2c676a76ef2885c9102aeaea874c361dbcaf61
David Wood <david@davidtw.co>
2021-03-08 19:56:54 +0000

    Upgrade Plex to 1.22.0.4157.

--- nix/overlays/plex.nix
+++ nix/overlays/plex.nix
@@ -3,11 +3,11 @@ self: super:
 {
   plexPassRaw = super.plexRaw.overrideAttrs (
     old: rec {
-      version = "1.21.3.4046-3c1c83ba4";
+      version = "1.22.0.4157-d1699ac4a";
       name = "${old.pname}-${version}";
       src = super.fetchurl {
         url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        sha256 = "1fywpkchpc726a66q7xpqrn92g73v4941df19glscrrvy7808f8n";
+        sha256 = "1p4yffah9m4d2apd67xv10ll4sv46mfixc9c4rg416v8lpyzga15";
       };
     }
   );
