3857c5c23ad80b0d7d1a1eba640500f161b05880
David Wood <david@davidtw.co>
2021-03-08 19:36:01 +0000

    Add nofork to SSH forwarding.

--- home/configs/gnupg.nix
+++ home/configs/gnupg.nix
@@ -187,7 +187,7 @@ in
       ss -a | grep -q $SSH_AUTH_SOCK
       if [ $? -ne 0 ]; then
         rm -f $SSH_AUTH_SOCK
-        (setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/.gnupg/wsl2-ssh-pageant.exe" >/dev/null 2>&1 &)
+        (setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/.gnupg/wsl2-ssh-pageant.exe",nofork >/dev/null 2>&1 &)
       fi
     '';
 
