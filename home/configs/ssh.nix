c08ef61827bd736c44176124a3c7aa77c310c84d
David Wood <david@davidtw.co>
2021-10-22 10:20:59 +0100

    Add intrepid host to SSH config.

--- home/configs/ssh.nix
+++ home/configs/ssh.nix
@@ -86,6 +86,10 @@ in
             ];
             user = "david";
           };
+          "dtw-intrepid" = {
+            hostname = "intrepid.dtw.io";
+            user = "davidw";
+          };
         };
       serverAliveInterval = 300;
     };
